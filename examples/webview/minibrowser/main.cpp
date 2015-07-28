f63b87473d78511a4433db06d3f12dc061670990
Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>
2015-07-28 15:37:07 +0200

    Make examples return -1 on load failures.
    Avoid hanging processes and empty windows in case some module is not
    installed.
    
    Change-Id: If25c9b5338a388b74e7a8603265079af6305a516
    Reviewed-by: Christian Stromme <christian.stromme@theqtcompany.com>

--- examples/webview/minibrowser/main.cpp
+++ examples/webview/minibrowser/main.cpp
@@ -113,6 +113,8 @@ int main(int argc, char *argv[])
     context->setContextProperty(QStringLiteral("initialHeight"), geometry.height());
 
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
 
     return app.exec();
 }
