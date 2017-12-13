qt/qtwebview

commit e4016a67bbffefed71a407494e249e978d212b3d

Darwin: When given a local file url we need to call loadFileURL
As loadRequest does not support local file urls we need to use
loadFileURL for this case instead. This gets given the url for the file
and one to cover the location that supporting files can be read from
too.

Change-Id: I13c3a8aabd030a222d2aac7acb236e1ff528a0ee
Reviewed-by: Jake Petroules <jake.petroules@qt.io>

src/webview/qwebview_darwin.mm
@@ -253,7 +253,14 @@ void QDarwinWebViewPrivate::setUrl(const QUrl &url)
 {
     if (url.isValid()) {
         requestFrameCount = 0;
-        [wkWebView loadRequest:[NSURLRequest requestWithURL:url.toNSURL()]];
+        if (!url.isLocalFile()) {
+            [wkWebView loadRequest:[NSURLRequest requestWithURL:url.toNSURL()]];
+        } else {
+            // We need to pass local files via loadFileURL and the read access should cover
+            // the directory that the file is in, to facilitate loading referenced images etc
+            [wkWebView loadFileURL:url.toNSURL()
+           allowingReadAccessToURL:QUrl(url.toString(QUrl::RemoveFilename)).toNSURL()];
+        }
     }
 }
 

commit 4d4b118c1294d96741d71a775ac1c57b5acb96bb

Fix the build with Xcode 9 and unguarded availability errors turned on
This fixes a regression introduced in
e4016a67bbffefed71a407494e249e978d212b3d.

Task-number: QTBUG-65075
Change-Id: I5723ba04ce02842c10aaa4b2ae5dd042f8beabf0
Reviewed-by: Gabriel de Dietrich <gabriel.dedietrich@qt.io>

src/webview/qwebview_darwin.mm
@@ -40,6 +40,7 @@
 #include "qtwebviewfunctions.h"
 #include "qtwebviewfunctions_p.h"
 
+#include <QtCore/private/qglobal_p.h>
 #include <QtCore/qdatetime.h>
 #include <QtCore/qmap.h>
 #include <QtCore/qvariant.h>
@@ -253,14 +254,19 @@ void QDarwinWebViewPrivate::setUrl(const QUrl &url)
 {
     if (url.isValid()) {
         requestFrameCount = 0;
-        if (!url.isLocalFile()) {
-            [wkWebView loadRequest:[NSURLRequest requestWithURL:url.toNSURL()]];
-        } else {
+
+#if QT_MACOS_IOS_PLATFORM_SDK_EQUAL_OR_ABOVE(101100, 90000)
+        if (url.isLocalFile()) {
             // We need to pass local files via loadFileURL and the read access should cover
             // the directory that the file is in, to facilitate loading referenced images etc
-            [wkWebView loadFileURL:url.toNSURL()
-           allowingReadAccessToURL:QUrl(url.toString(QUrl::RemoveFilename)).toNSURL()];
+            if (__builtin_available(macOS 10.11, iOS 9, *)) {
+                [wkWebView loadFileURL:url.toNSURL()
+               allowingReadAccessToURL:QUrl(url.toString(QUrl::RemoveFilename)).toNSURL()];
+                return;
+            }
         }
+#endif
+        [wkWebView loadRequest:[NSURLRequest requestWithURL:url.toNSURL()]];
     }
 }
 

src/webview/webview.pro
@@ -1,7 +1,7 @@
 TARGET = QtWebView
 
 QT =
-QT_FOR_PRIVATE = quick-private gui-private
+QT_FOR_PRIVATE = quick-private core-private gui-private
 
 include($$PWD/webview-lib.pri)
 