5979af58d780d86c92e93ef5fcc379fb1799c756
Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>
2026-02-03 11:45:53 +0100

    Add settings to C++ example
    Adds a settings dropdown with checkable settings. Includes
    code to check if the setting actually took effect.
    
    Pick-to: 6.11
    Change-Id: Iec15389b7df6a5383a142a023586fa92cebe61dd
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- examples/webview/webviewbrowser/browserwindow.h
+++ examples/webview/webviewbrowser/browserwindow.h
@@ -2,12 +2,14 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
 #include <QtWidgets/qmainwindow.h>
+#include <QtWebView/qwebviewsettings.h>
 
 QT_BEGIN_NAMESPACE
 class QAction;
 class QLineEdit;
 class QProgressBar;
 class QToolBar;
+class QToolButton;
 class QWebView;
 class QWebViewLoadingInfo;
 QT_END_NAMESPACE
@@ -23,16 +25,26 @@ public Q_SLOTS:
     void onLoadingChanged(const QWebViewLoadingInfo &loadingInfo);
     void onTitleChanged(const QString &title);
     void onLoadProgressChanged(int loadProgress);
+    void onSettingToggled(QAction *action, QWebViewSettings::WebAttribute attribute);
     void onUrlInput();
 
 private:
+    void addSettingToMenu(QAction *action, QWebViewSettings::WebAttribute setting);
+
     QWebView *webView;
+
     QToolBar *toolBar;
     QLineEdit *lineEdit;
     QProgressBar *progressBar;
+    QToolButton *settingsMenuButton;
 
     QAction *backAction;
     QAction *forwardAction;
     QAction *reloadAction;
     QAction *stopAction;
+
+    QAction *localStorageAction;
+    QAction *javaScriptAction;
+    QAction *allowFileAccessAction;
+    QAction *localContentsCanAccessFileUrlsAction;
 };
